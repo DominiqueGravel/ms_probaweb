3c7a36c43fb2cd2d41e3ee4bcf54e473572f97cc
SteveViss <s.vissault@yahoo.fr>
2016-05-20 12:14:40 -0400

    Add parallel option

--- analysis/scripts/data_processing/reformat_salix.r
+++ analysis/scripts/data_processing/reformat_salix.r
@@ -7,14 +7,9 @@
 ##################################################################
 
 rm(list=ls())
-#setwd("/Users/DGravel/Documents/Manuscripts/Inprep/ms_probaweb")
+
 # Load the data
-
-# Original data from mangal
-#load("analysis/data/salix_int.Rdata")
-
-# Instead work with the original Roslin dataset
-datakop <- read.csv("analysis/data/kopeleke_data.csv",dec=",")
+datakop <- read.csv("data/kopeleke_data.csv",dec=",")
 HP = datakop[,c(9,10,11,1,19,32)]
 #HP = subset(HP, HP[,6]!="no parasitoid or inquiline")
 SH = datakop[,c(9,10,11,1,29,19)]
@@ -22,7 +17,7 @@ names(HP) = c("site","lat","lon","year","from","to")
 names(SH) = c("site","lat","lon","year","from","to")
 SH[,5] = paste("Salix",SH[,5])
 salix_int = rbind(HP,SH)
-save(salix_int, file = "analysis/data/salix_int_direct.Rdata")
+save(salix_int, file = "./data/salix_int_direct.Rdata")
 
 # Get the IDs
 sites = as.character(salix_int[,1])
@@ -42,7 +37,7 @@ HP_pairs = cbind(expand.grid(ID_H, ID_P), type = "GP")
 pairs = rbind(SH_pairs, HP_pairs)
 
 # Record the pair IDs
-save(pairs, file="analysis/data/pairs.Rdata")
+save(pairs, file="./data/pairs.Rdata")
 
 # Look at co-occurrence for pairs of salix ang galls
 pairs0_SH = data.frame(
@@ -60,22 +55,62 @@ XS = numeric(nrow(pairs_SH))
 XH = numeric(nrow(pairs_SH))
 LSH = numeric(nrow(pairs_SH))
 
-# Loop around all observations of interactions
-for(x in 1:nrow(SH)) {
+if(parallel == TRUE){
+	
+	# Init parrallele
+	#0. Packages loading
+	library(doParallel)
 
-	# Get the sites where the salix is present
-	XS[which(pairs_SH$sites_ID == SH[x,1] & pairs_SH$ID_S == SH[x,5])] = 1
+	#1. Init the cluster of SOCKETS
+	# Get the number of cores by node
+	nCores <- detectCores()
 
-	# Get the sites where the gall is present
-	XH[which(pairs_SH$sites_ID == SH[x,1] & pairs_SH$ID_H == SH[x,6])] = 1
-		
-	# Get the sites where the gall and the salix interact
-	LSH[which(pairs_SH$sites_ID == SH[x,1] & pairs_SH$ID_S == SH[x,5] & pairs_SH$ID_H == SH[x,6])] = 1
+	# Open the cluster
+	cl <- makeCluster(nCores)
+	# We replicated the addresses nCores times.
+	registerDoParallel(cl)
 
+	# Loop around all observations of interactions
+	id_xs <- foreach(x=1:nrow(SH),.combine=c) %dopar% {
+		# Get the sites where the salix is present
+		 return(which(pairs_SH$sites_ID == SH[x,1] & pairs_SH$ID_S == SH[x,5]))
+	}
+	
+	id_xh <- foreach(x=1:nrow(SH),.combine=c) %dopar% {
+		# Get the sites where the salix is present
+		 return(which(pairs_SH$sites_ID == SH[x,1] & pairs_SH$ID_H == SH[x,6]))
+	}
+	
+	id_lsh <- foreach(x=1:nrow(SH),.combine=c) %dopar% {
+		# Get the sites where the salix is present
+		 return(which(pairs_SH$sites_ID == SH[x,1] & pairs_SH$ID_S == SH[x,5] & pairs_SH$ID_H == SH[x,6]))
+	}
+	
+	# convert matches to 1
+	XS[id_xs] = 1
+	XH[id_xh] = 1
+	LSH[id_lsh] = 1
+	
+} else{
+	
+	# Loop around all observations of interactions
+	for(x in 1:nrow(SH)) {
+
+		# Get the sites where the salix is present
+		XS[which(pairs_SH$sites_ID == SH[x,1] & pairs_SH$ID_S == SH[x,5])] = 1
+
+		# Get the sites where the gall is present
+		XH[which(pairs_SH$sites_ID == SH[x,1] & pairs_SH$ID_H == SH[x,6])] = 1
+			
+		# Get the sites where the gall and the salix interact
+		LSH[which(pairs_SH$sites_ID == SH[x,1] & pairs_SH$ID_S == SH[x,5] & pairs_SH$ID_H == SH[x,6])] = 1
+
+	}
 }
-data = cbind(pairs_SH,XS,XH,LSH)
-save(data, file = "analysis/data/SH.Rdata")
 
+
+data = cbind(pairs_SH,XS,XH,LSH)
+save(data, file = "./data/SH.Rdata")
 
 # Look at co-occurrence for pairs of galls and parasitoids
 pairs0_HP= data.frame(
@@ -93,30 +128,54 @@ XP = numeric(nrow(pairs_HP))
 LHP = numeric(nrow(pairs_HP))
 
 # Loop around all observations of interactions
-for(x in 1:nrow(HP)) {
+if(parallel==TRUE){
+	
+	id_xh2 <- foreach(x=1:nrow(HP),.combine=c) %dopar% {
+		# Get the sites where the salix is present
+		 return(which(pairs_HP$sites_ID == HP[x,1] & pairs_HP$ID_H == HP[x,5]))
+	}
+	
+	id_xp <- foreach(x=1:nrow(HP),.combine=c) %dopar% {
+			# Get the sites where the gall is present
+		 return(which(pairs_HP$sites_ID == HP[x,1] & pairs_HP$ID_P == HP[x,6]))
+	}
+	
+	id_lhp <- foreach(x=1:nrow(HP),.combine=c) %dopar% {
+		# Get the sites where the gall and the salix interact
+		 return(which(pairs_HP$sites_ID == HP[x,1] & pairs_HP$ID_H == HP[x,5] & pairs_HP$ID_P == HP[x,6]))
+	}
+	
+	# convert matches to 1
+	XH2[id_xh2] = 1
+	XP[id_xp] = 1
+	LHP[id_lhp] = 1
+	
+	stopCluster(cl)
+	
+} else{
+	
+	for(x in 1:nrow(HP)) {
 
-	# Get the sites where the salix is present
-	XH2[which(pairs_HP$sites_ID == HP[x,1] & pairs_HP$ID_H == HP[x,5])] = 1
+		# Get the sites where the salix is present
+		XH2[which(pairs_HP$sites_ID == HP[x,1] & pairs_HP$ID_H == HP[x,5])] = 1
 
-	# Get the sites where the gall is present
-	XP[which(pairs_HP$sites_ID == HP[x,1] & pairs_HP$ID_P == HP[x,6])] = 1
-		
-	# Get the sites where the gall and the salix interact
-	LHP[which(pairs_HP$sites_ID == HP[x,1] & pairs_HP$ID_H == HP[x,5] & pairs_HP$ID_P == HP[x,6])] = 1
+		# Get the sites where the gall is present
+		XP[which(pairs_HP$sites_ID == HP[x,1] & pairs_HP$ID_P == HP[x,6])] = 1
+			
+		# Get the sites where the gall and the salix interact
+		LHP[which(pairs_HP$sites_ID == HP[x,1] & pairs_HP$ID_H == HP[x,5] & pairs_HP$ID_P == HP[x,6])] = 1
 
-	cat(x, " / ", nrow(HP), '\n')
+	}
 }
 
+
 data_HP = data.frame(pairs_HP = pairs_HP, XH2 = XH2, XP = XP, LHP = LHP)
-save(data_HP, file = "analysis/data/HP.Rdata")
+save(data_HP, file = "./data/HP.Rdata")
 
-
-# 
-load("analysis/data/HP.Rdata")
+load("./data/HP.Rdata")
 XH2 = data_HP$XH2
 XP = data_HP$XP
 LHP = data_HP$LHP
-
 
 # Combine the sets
 names(pairs_SH) = c("sites_ID", "from", "to", "type")
@@ -127,24 +186,20 @@ Xj = c(XH,XP)
 Xij = Xi*Xj
 Lij = c(LSH,LHP)
 
-
 ###################################################
-# Load packages
+# Get climatic data
 library(dismo)
 library(raster)
-bclim = brick("analysis/data/bioclim.grd")
+bclim = brick("./data/bioclim.grd")
 salix_climate = extract(bclim, salix_int[,c('lon', 'lat')])
 salix_climate = data.frame(salix_int, salix_climate)
-save(salix_climate, file="analysis/data/salix_climate.Rdata")
-
-
-
+save(salix_climate, file="./data/salix_climate.Rdata")
 
 # Subset the data to have one line per site
 sub_salix_climate = salix_climate[match(sites_ID,sites),c(1,7:25)]
 
 # Compute the PCA
-PCA = princomp(sub_salix_climate[,2:20],cor = TRUE)
+PCA = princomp(na.omit(sub_salix_climate[,2:20]),cor = TRUE)
 summary(PCA)
 
 # Join the scores with the climate data
@@ -154,24 +209,9 @@ sub_salix_climate = cbind(sub_salix_climate,PCA$scores)
 match_pairs_climate = match(as.character(pairs[,1]), 
 	as.character(sub_salix_climate[,1]))
 
-#match_pairs_climate = subset(match_pairs_climate0, 
-#	is.na(match_pairs_climate0) == FALSE)
-
 expand_climate = sub_salix_climate[match_pairs_climate,]
 
-#data = data.frame(pairs = pairs[is.na(match_pairs_climate0) == FALSE,], 
-#Xi=Xi[is.na(match_pairs_climate0) == FALSE], 
-#Xj=Xj[is.na(match_pairs_climate0) == FALSE], 
-#Xij=Xij[is.na(match_pairs_climate0) == FALSE], 
-#Lij=Lij[is.na(match_pairs_climate0) == FALSE], climate = expand_climate)
-
+# Final dataset
 data = data.frame(pairs = pairs, Xi=Xi, Xj=Xj, Xij=Xij, Lij=Lij, climate = expand_climate)
-
-# Drop the NAs
 data = subset(data,is.na(data$climate.bio1)==FALSE)
-
-save(data,file = "analysis/data/expand_data.Rdata")
-
-
-
-
+save(data,file = "./data/expand_data.Rdata")
