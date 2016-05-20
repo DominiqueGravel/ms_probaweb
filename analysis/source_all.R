cf02f2b620201ef26843397f493b076efe04941f
SteveViss <s.vissault@yahoo.fr>
2016-05-20 12:14:02 -0400

    set relative path

---
+++ analysis/source_all.R
@@ -0,0 +1,14 @@
+# init parallel option
+parallel = TRUE
+
+# Process the data
+source("./scripts/data_processing/reformat_salix.r")
+source("./scripts/data_processing/split_data.r")
+
+# Get the results
+source("./scripts/results/1_metaweb_sampling.r")
+source("./scripts/results/3_example_pair.r")
+source("./scripts/results/4_map_pair.r")
+source("./scripts/results/5_metaweb_holes.r")
+source("./scripts/results/6_map_all.r")
+source("./scripts/results/table_all_models.r")
