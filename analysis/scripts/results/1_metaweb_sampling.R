d470448adbf8c578a0695063c6685c85392d9825
Dominique Gravel <grad3002@dbio-graved-01i.DInf.fsci.usherbrooke.ca>
2016-05-24 12:05:22 -0400

    Final edits to Fig 1

--- analysis/scripts/results/1_metaweb_sampling.R
+++ analysis/scripts/results/1_metaweb_sampling.R
@@ -25,12 +25,8 @@ Sall = length(unique_ID_all)
 DF = data.frame(sites = data$pairs.sites_ID, IDi = IDi, IDj = IDj, Xi=data$Xi, Xj=data$Xj, Xij = data$Xij, Lij = data$Lij)
 
 #########################################################
-# Network sampling
-#########################################################
-
 # Adjacency matrix for the metaweb
-# Loop around all pairs of species to test if there are interactions
-# Put everything in a square matrix
+#########################################################
 
 mw = data.frame(matrix(0, nr = Sall, nc = Sall))
 names(mw) = unique_ID_all
@@ -56,10 +52,12 @@ for(n in 1:nrow(pairs)) {
 }
 
 #########################################
-# Compute the local network for one site
+# Adjacency matrix for one site
 #########################################
 
-lw = matrix(0, nr = Sall, nc = Sall)
+lw = data.frame(matrix(0, nr = Sall, nc = Sall))
+names(lw) = unique_ID_all
+row.names(lw) = unique_ID_all
 DF_split_sites = split(DF,DF$sites)
 site_index = 369 # Pick one that has a decent number of links
 
@@ -82,13 +80,14 @@ for(n in 1:nrow(pairs)) {
 }
 
 #########################################################
-# Convert the metaweb to igraph
+# Convert the adjacency matrices to igraph
 #########################################################
 
 library(igraph)
-g = graph.incidence(mw,add.names=NULL, mode = "undirected")
+g_mw = graph.adjacency(as.matrix(mw))
+g_lw = graph.adjacency(as.matrix(lw))  
 
-#########################################################
+########################################################
 # Set the layer attribute for every node
 #########################################################
 
@@ -96,21 +95,12 @@ salix_ID = as.character(unique(pairs[pairs$pairs.type=="SH",1]))
 gall_ID = as.character(unique(pairs[pairs$pairs.type=="HP",1]))
 par_ID = as.character(unique(pairs[pairs$pairs.type=="HP",2]))
 
-V(g)$layer[V(g)$name %in% salix_ID & dlw == 0] = 1.5
-V(g)$layer[V(g)$name %in% salix_ID & dlw != 0] = 2
-
-V(g)$layer[V(g)$name %in% gall_ID & dlw != 0] = 3.5
-V(g)$layer[V(g)$name %in% gall_ID & dlw == 0] = 4
-
-V(g)$layer[V(g)$name %in% par_ID & dlw != 0] = 5.5
-V(g)$layer[V(g)$name %in% par_ID & dlw ==0] = 6
-
-#V(g)$layer[V(g)$name %in% salix_ID] = 1
-#V(g)$layer[V(g)$name %in% gall_ID] = 2
-#V(g)$layer[V(g)$name %in% par_ID] = 3
+V(g_mw)$layer[V(g_mw)$name %in% salix_ID] = 1
+V(g_mw)$layer[V(g_mw)$name %in% gall_ID] = 2
+V(g_mw)$layer[V(g_mw)$name %in% par_ID] = 3
 
 #########################################################
-# Plot the metaweb
+# Plot the network
 #########################################################
 
 dev.new(width = 6.5, height = 20)
@@ -123,32 +113,28 @@ layout.k_partite <- function(g) {
   l
 }
 
-# Color code the nodes that are present
+# Color code the nodes for the different layers & put in grey the ones that are absent
+library(RColorBrewer)
+pal <- brewer.pal(3, "Dark2")
 ecol <- rgb(0.4, 0.4, 0.4, alpha=0.4)
 wcol <- rgb(0.4, 0.4, 0.4)
 
 dlw = apply(lw,1,sum)
-library(RColorBrewer)
-pal <- brewer.pal(3, "Dark2")
-V(g)$color[V(g)$name %in% salix_ID] = pal[1]
-V(g)$color[V(g)$name %in% gall_ID] = pal[2]
-V(g)$color[V(g)$name %in% par_ID] = pal[3]
-V(g)$color[dlw==0] = wcol
+V(g_mw)$size = 3
+V(g_mw)$size[dlw!=0] = 5
+V(g_mw)$color[V(g_mw)$name %in% salix_ID] = brewer.pal(5, "Dark2")[5]
+V(g_mw)$color[V(g_mw)$name %in% gall_ID] = brewer.pal(5, "Dark2")[2]
+V(g_mw)$color[V(g_mw)$name %in% par_ID] = brewer.pal(5, "Set1")[1]
+V(g_mw)$color[dlw==0] = wcol
 
-V(g)$size = 3
-V(g)$size[dlw!=0] = 5
-
-
-# Color code the links that are present
-
+# Same for the edges
+el_mw = apply(get.edgelist(g_mw), 1, paste, collapse = "-")
+el_lw = apply(get.edgelist(g_lw), 1, paste, collapse = "-")
+E(g_mw)$width = ifelse(el_mw %in% el_lw, 1, 0.3)
+E(g_mw)$color = ifelse(el_mw %in% el_lw, "darkblue", ecol)
 
 # Plot the network
-plot(g, layout = layout.k_partite(g), vertex.label=NA, edge.color = ecol, edge.arrow.mode=0, edge.width=0.3, margin = c(-0.075,-0.075,-0.075,-0.075), asp=0, vertex.frame.color = NA)
+plot(g_mw, layout = layout.k_partite(g_mw), vertex.label=NA, edge.arrow.mode=0, margin = c(-0.075,-0.075,-0.075,-0.075), asp=0, vertex.frame.color = NA)
 
 # Save file
-dev.copy2pdf(file = "./figures/metaweb_sampling.pdf")
-
-
-
-
-
+dev.copy2pdf(file = "./figures/mw_sampling.pdf")
